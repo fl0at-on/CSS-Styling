fae02366d911b6185c2aa6a64b82d975a91123a3
fl0at-on <megaw.mark@gmail.com>
2019-01-22 22:02:48 -0600

    Adding sidepanel

--- src/index.js
+++ src/index.js
@@ -14,24 +14,39 @@ function App() {
       </nav>
 
       <div className="container">
-        <h1 className="grid-header">Input Screen</h1>
+        <div className="side-menu">
+          <div className="side-menu-header">Navigation</div>
+          <div className="side-menu-item">ITEM 1</div>
+          <div className="side-menu-item">ITEM 2</div>
+          <div className="side-menu-item">ITEM 3</div>
+        </div>
+        <h2 className="grid-header">Input Screen</h2>
+
         <div className="grid-items">
-          <h2 className="header">Fields</h2>
+          <h3 className="header">Fields</h3>
+          <div className="grid-item">
+            <div className="grid-label">Field 1</div>
+            <input className="grid-input" type="text" />
+          </div>
+          <div className="grid-item">
+            <div className="grid-label">Field 2</div>
+            <input className="grid-input" type="text" />
+          </div>
 
-          <div className="grid-label">Field 1</div>
-          <input className="grid-input" type="text" />
+          <div className="grid-item">
+            <div className="grid-label">Field 3</div>
+            <input className="grid-input" type="text" />
+          </div>
 
-          <div className="grid-label">Field 2</div>
-          <input className="grid-input" type="text" />
+          <div className="grid-item">
+            <div className="grid-label">Field 4</div>
+            <input className="grid-input" type="text" />
+          </div>
 
-          <div className="grid-label">Field 3</div>
-          <input className="grid-input" type="text" />
-
-          <div className="grid-label">Field 4</div>
-          <input className="grid-input" type="text" />
-
-          <div className="grid-label">Field 5</div>
-          <input className="grid-input" type="text" />
+          <div className="grid-item">
+            <div className="grid-label">Field 5</div>
+            <input className="grid-input" type="text" />
+          </div>
 
           <div className="submit">
             <button className="btn-cancel">CANCEL</button>
